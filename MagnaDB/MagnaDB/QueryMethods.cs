6437791abd5eb01df3f8e5b2ec297d456b0029d5
emt38 <ezequieltheboss64@gmail.com>
2016-10-05 01:25:44 -0400

    Completed QueryMethods

--- MagnaDB/MagnaDB/QueryMethods.cs
+++ MagnaDB/MagnaDB/QueryMethods.cs
@@ -16,7 +16,7 @@ namespace MagnaDB
             {
                 if (query[x] == ',' && query[x + 1] == ',')
                 {
-                    query = query.Substring(0, x + 1) + "null" + query.Substring(x + 1);
+                    query = query.Insert(x + 1, "NULL");
                 }
             }
 
@@ -33,6 +33,7 @@ namespace MagnaDB
                 }
                 catch (Exception ex)
                 {
+                    connection.Close();
                     throw ex;
                 }
             }
@@ -44,7 +45,7 @@ namespace MagnaDB
             {
                 if (query[x] == ',' && query[x + 1] == ',')
                 {
-                    query = query.Substring(0, x + 1) + "null" + query.Substring(x + 1);
+                    query = query.Insert(x + 1, "NULL");
                 }
             }
 
@@ -69,12 +70,13 @@ namespace MagnaDB
             {
                 if (query[x] == ',' && query[x + 1] == ',')
                 {
-                    query = query.Substring(0, x + 1) + "null" + query.Substring(x + 1);
+                    query = query.Insert(x + 1, "NULL");
                 }
             }
 
             using (SqlCommand command = new SqlCommand(query, transaction.Connection))
             {
+                command.Transaction = transaction;
                 try
                 {
                     int c = command.ExecuteNonQuery();
@@ -94,7 +96,7 @@ namespace MagnaDB
             {
                 if (query[x] == ',' && query[x + 1] == ',')
                 {
-                    query = query.Substring(0, x + 1) + "null" + query.Substring(x + 1);
+                    query = query.Insert(x + 1, "NULL");
                 }
             }
 
@@ -111,6 +113,7 @@ namespace MagnaDB
                 }
                 catch (Exception ex)
                 {
+                    connection.Close();
                     throw ex;
                 }
             }
@@ -122,7 +125,7 @@ namespace MagnaDB
             {
                 if (query[x] == ',' && query[x + 1] == ',')
                 {
-                    query = query.Substring(0, x + 1) + "null" + query.Substring(x + 1);
+                    query = query.Insert(x + 1, "NULL");
                 }
             }
 
@@ -147,17 +150,355 @@ namespace MagnaDB
             {
                 if (query[x] == ',' && query[x + 1] == ',')
                 {
-                    query = query.Substring(0, x + 1) + "null" + query.Substring(x + 1);
+                    query = query.Insert(x + 1, "NULL");
                 }
             }
 
             using (SqlCommand command = new SqlCommand(query, transaction.Connection))
             {
+                command.Transaction = transaction;
                 try
                 {
                     int c = await command.ExecuteNonQueryAsync();
 
                     return c > 0;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static object DoScalar(string query, string connectionString)
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    connection.Close();
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static object DoScalar(string query, SqlConnection connection)
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    object result = command.ExecuteScalar();
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static object DoScalar(string query, SqlTransaction transaction)
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, transaction.Connection))
+            {
+                command.Transaction = transaction;
+                try
+                {
+                    object result = command.ExecuteScalar();
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static async Task<object> DoScalarAsync(string query, string connectionString)
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    object result = await command.ExecuteScalarAsync();
+                    connection.Close();
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    throw ex;
+                }
+            }
+        }
+
+        public static async Task<object> DoScalarAsync(string query, SqlConnection connection)
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    object result = await command.ExecuteScalarAsync();
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static async Task<object> DoScalarAsync(string query, SqlTransaction transaction)
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, transaction.Connection))
+            {
+                command.Transaction = transaction;
+                try
+                {
+                    object result = await command.ExecuteScalarAsync();
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static DataTable TableMake(string query, string connectionString, string tableName = "")
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        connection.Close();
+                        DataTable temp = new DataTable(tableName);
+                        temp.Load(reader);
+                        return temp;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    throw ex;
+                }
+            }
+        }
+
+        public static DataTable TableMake(string query, SqlConnection connection, string tableName = "")
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable temp = new DataTable(tableName);
+                        temp.Load(reader);
+                        return temp;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static DataTable TableMake(string query, SqlTransaction transaction, string tableName = "")
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, transaction.Connection))
+            {
+                command.Transaction = transaction;
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable temp = new DataTable(tableName);
+                        temp.Load(reader);
+                        return temp;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static async Task<DataTable> TableMakeAsync(string query, string connectionString, string tableName = "")
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        connection.Close();
+                        DataTable temp = new DataTable(tableName);
+                        temp.Load(reader);
+                        return temp;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    throw ex;
+                }
+            }
+        }
+
+        public static async Task<DataTable> TableMakeAsync(string query, SqlConnection connection, string tableName = "")
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        DataTable temp = new DataTable(tableName);
+                        temp.Load(reader);
+                        return temp;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public static async Task<DataTable> TableMakeAsync(string query, SqlTransaction transaction, string tableName = "")
+        {
+            for (int x = 0; x < (query.Length - 1); x++)
+            {
+                if (query[x] == ',' && query[x + 1] == ',')
+                {
+                    query = query.Insert(x + 1, "NULL");
+                }
+            }
+
+            using (SqlCommand command = new SqlCommand(query, transaction.Connection))
+            {
+                command.Transaction = transaction;
+                try
+                {
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        DataTable temp = new DataTable(tableName);
+                        temp.Load(reader);
+                        return temp;
+                    }
                 }
                 catch (Exception ex)
                 {
