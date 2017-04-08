9878bca7221d23277924c8b2828f18b08702dd0a
emt38 <ezequieltheboss64@gmail.com>
2017-04-08 01:55:34 -0400

    Minor Changes

---
+++ MagnaDB/MagnaDB/SampleRelationship.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagnaDB
+{
+    /// <summary>
+    /// You can create the tables in a SQL Server to try this OUT :)
+    /// </summary>
+    class Foo
+    {
+        public int IdFoo { get; set; }
+
+        [ForeignRelation("<{0}.IdFoo> = {1}.IdFoo")]
+        public IEnumerable<Bar> Bars { get; set; }
+    }
+
+    class Bar
+    {
+        public int IdBar { get; set; }
+        public int IdFoo { get; set; }
+
+        [ForeignRelation("<{0}.IdFoo> = {1}.IdFoo")]
+        public Foo MyFoo { get; set; }
+    }
+}
